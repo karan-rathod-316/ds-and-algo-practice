7fba7d33668d98bd14695fd5e6f26078c786e402
Karan Rathod <karan.altcampus@gmail.com>
2022-03-01 14:23:43 +0530

    Solve character that is most commonly used in the string

--- main.js
+++ main.js
@@ -41,5 +41,34 @@ function reverseInt(n) {
     return reversedInt
   }
 
+// 4. Return the character that is most commonly used in the string
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
 
+function maxChar(str) {
+    
+    let strObject = {};
+    let result = {char:0, val:0};
+
+    for(let char of str) {
+        if(!strObject[char]) {
+            strObject[char] = 1
+        } else {
+            strObject[char]++
+        }    
+    } 
+
+    for(let char in strObject) {
+        if(strObject[char] > result.val) {
+            result.char = char;
+            result.val = strObject[char] 
+        }
+    } 
+    return result.char
+}
+maxChar("abcdefghijklmnaaaaa")
 
